4bc42043adc45ce07033222125e59b86473d8317
SaharshPatel24 <srpatel56@myseneca.ca>
2022-07-30 11:50:47 -0400

    Implemented search query

--- server/index.ts
+++ server/index.ts
@@ -1,13 +1,32 @@
-import express, { Express, Request, Response } from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
+import axios from 'axios';
 
 dotenv.config();
 
-const app: Express = express();
+const app: express.Express = express();
+
 const port = process.env.PORT || 5000;
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: express.Request, res: express.Response) => {
   res.send('Express + TypeScript Server');
+});
+
+app.get('/search', async (req: express.Request, res: express.Response) => {
+  let { query } = req.query;
+
+  if (!query) {
+    return res.status(400).send("Missing Query string");
+  }
+
+  try {
+    let response = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`);
+
+    return res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json(error);
+    throw new Error(String(error));
+  }
 });
 
 
