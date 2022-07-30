767318bcc0e6a80cffc93e770d290862d50249aa
SaharshPatel24 <srpatel56@myseneca.ca>
2022-07-30 12:03:42 -0400

    implemented endpoint to fetch coin information

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import dotenv from 'dotenv';
 import axios from 'axios';
 
@@ -21,6 +21,23 @@ app.get('/search', async (req: express.Request, res: express.Response) => {
 
   try {
     let response = await axios.get(`https://api.coingecko.com/api/v3/search?query=${query}`);
+
+    return res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json(error);
+    throw new Error(String(error));
+  }
+});
+
+app.get('/coin-info', async (req: express.Request, res: express.Response) => {
+  let { query } = req.query;
+
+  if (!query) {
+    return res.status(400).send("Missing Query string");
+  }
+
+  try {
+    let response = await axios.get(`https://api.coingecko.com/api/v3/coins/${query}`);
 
     return res.status(200).json(response.data);
   } catch (error) {
