e2c89ce3b2d84a9b048b023d3865ae7c141e88dc
SaharshPatel24 <srpatel56@myseneca.ca>
2022-07-30 10:36:43 -0400

    added search field in App.tsx

--- client-side/src/App.tsx
+++ client-side/src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [Query, setQuery] = useState('');
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="form-search" method="get" action="#">
+        <input type="search" name="search" onChange={event => setQuery(event.target.value)} placeholder="search your crypto here ...." />
+        <button type="submit">Search</button>
+      </form>
     </div>
   );
 }
